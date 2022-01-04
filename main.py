13439d9d5c08f16383f8682c42a88352c334d2f5
unknown <savymarcos@gmail.com>
2022-01-03 21:32:36 -0300

    Actualice el codigo segun jupyter

---
+++ main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
