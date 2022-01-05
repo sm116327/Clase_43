f70e7eb3b8084afb7bbbfa325fbf42dd14d114e6
Marcos S <savymarcos@gmail.com>
2022-01-05 19:43:45 -0300

    "Ultimos cambios"

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # This is a sample Python script.
 
 def print_hi(name):
+    hola=10
+    # import pdb;pdb.set_trace()
+    Hello=50
+    Chau = hola + Hello
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
